e27a316e4eb1e6c3e1076b76f870e9e7c1cecdfe
lucktroy <jbzhang86@gmail.com>
2014-05-01 01:17:47 +0800

    default update all

--- runTest.sh
+++ runTest.sh
@@ -4,4 +4,4 @@ cd /tmp && rm -rf regression-deep-learning && git clone https://github.com/luckt
 cd -
 #sudo docker run -v /tmp/regression-deep-learning:/opt/Test/regression-deep-learning lucktroy/pylearn2 sh /opt/Test/regression-deep-learning/example-pylearn2/runMe.sh
 
-sudo docker run -v /tmp/regression-deep-learning:/media lucktroy/pylearn2 sh -c 'cd /media/example-pylearn2 && sh runMe2.sh'
+sudo docker run -v /tmp/regression-deep-learning:/media lucktroy/pylearn2 sh -c 'cd /media/example-pylearn2 && sh runTest.sh'
